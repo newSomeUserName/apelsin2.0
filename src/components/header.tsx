a1ce4dea8db8d1eeac00f0236b5882d048e1de21
German Takhirov <gleb200360@gmail.com>
2026-02-26 18:25:24 +0900

    second commit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ export function Header()
 {
     return <div className="w-full">
         <h1 className="w-full flex justify-center">
-            <Image src={"/logo.jpg"} width={300} height={90} alt="logo" className="p-2 bg-white rounded-2xl"></Image>
+            <Image src={"./logo.jpg"} width={300} height={90} alt="logo" className="p-2 bg-white rounded-2xl"></Image>
         </h1>
     </div>
 }
